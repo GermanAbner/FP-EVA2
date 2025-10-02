8ee77819152f36c6f7ef93960cea89eae204588c
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-10-02 16:02:19 -0700

    EVA2_Practica3_25550584

---
+++ EVA2_Practica3_25550584/src/eva2_practica3_25550584/EVA2_Practica3_25550584.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica3_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA2_Practica3_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String cade = "Lunes";
+        switch (cade){
+                case "Lunes":
+                System.out.println(1);
+                break;
+                case "Martes":
+                    System.out.println(2);
+                    break;
+                default:
+                    System.out.println("Dia no existente");
+        }
+        
+    }
+    
+}

19c130c7e934f7351317dd98503f35a1d81d2029
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-10-02 16:02:46 -0700

    EVA2_Practica4_25550584

---
+++ EVA2_Practica4_25550584/src/eva2_practica4_25550584/EVA2_Practica4_25550584.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica4_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA2_Practica4_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        //Scanner captu = new Scanner(System.in);
+        
+        int arreglo[]= new int [5];//5 x 4 bytes = 20 bytes de peso MIJOOOOOOOOOOOOOOO
+        //PRIMER ELEMENTO DEL ARREGLO --> 0 CERO JAVA,C++
+        //ultimo elemento --> tamaño - 1
+        
+        arreglo [0]=100;
+        arreglo [1]=200;
+        arreglo [2]=300;
+        arreglo [3]=400;
+        arreglo [4]=500;
+        
+        
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+    }
+    
+}

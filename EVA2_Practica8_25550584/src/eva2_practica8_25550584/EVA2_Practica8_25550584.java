66ee50ffaf524caee32e8db8eea5a8271a6b8843
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-10-03 15:51:49 -0700

    EVA2_Practica8_25550584
    FOR

---
+++ EVA2_Practica8_25550584/src/eva2_practica8_25550584/EVA2_Practica8_25550584.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica8_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA2_Practica8_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        //3 PARAMETROS
+        // CUAL ES SU VARIABLE Y CUAÑ ES SI VALOR DE INICIO OMGOMGOMGOMGOMGOGM
+        // hasta a donde llegas (Condicion -->  tiene que ser verdadero)
+        //incremento o decremento locooooooouuuuuuuuuuuuuuuuuuuuuuu
+        // SUMAR O RESTAR VALORES papa
+        // OPERADOES: ALADIR 1 O RESTAR 1
+        // ++       --
+        //A++ SIGNIFICA SUMARLE 1 AL VALOR DE LA A
+        
+        System.out.println("**********************");
+        System.out.println("**********************");
+        System.out.println("**********************");
+        for(int a = 1; a <= 10;a++){
+            System.out.println("Hola Mundo!!" + "  " + a);
+        }
+        System.out.println("**********************");
+        System.out.println("**********************");
+        System.out.println("**********************");
+        
+        for(int a = 10; a >= 1 ;a--){
+            System.out.println("Hola Mundo!!" + "  " + a);
+        }
+        System.out.println("**********************");
+        System.out.println("**********************");
+        System.out.println("**********************");
+       }
+    
+}

7bc75c1fde5605ac1d624e75e9616972fe4390a0
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-10-15 15:52:23 -0700

    EVA2_Practica15_25550584

---
+++ EVA2_Practica15_.25550584/src/eva2_practica15_/pkg25550584/EVA2_Practica15_25550584.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica15_.pkg25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA2_Practica15_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     //ACUMULADOR
+     //++ -- FUNCIONAN DE 1 EN 1
+     //+= -= *= /=
+     int cuenta = 0;
+        System.out.println(cuenta);
+     cuenta = cuenta + 100;     //ACUMULADOR
+     //EL IGUAL EN JAVA ES LO QUE TE DE DE RESULTADO EN ESA OPERACION ASIGNALA AQUI
+     //ASIGNAR OMGGG
+        System.out.println(cuenta);
+     cuenta = cuenta + 1000;
+        System.out.println(cuenta);
+     cuenta = cuenta -  800; //cuenta -= 800;
+        System.out.println(cuenta);
+        cuenta += 2000;
+        System.out.println(cuenta);
+        
+        
+        
+    }
+    
+}

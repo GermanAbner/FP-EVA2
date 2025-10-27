b6710078c7055abd67b8a98a0990d98748c934c1
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-10-27 15:49:07 -0700

    Eva2_Practica13_25550584

---
+++ EVA2_Practica13_25550584/src/eva2_practica13_25550584/EVA2_Practica13_25550584.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica13_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA2_Practica13_25550584 {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int arreglo [] = new int [10];
+        arreglo [0] = 100;
+        arreglo [1] = 200;
+        arreglo [2] = 300;
+        arreglo [3] = 400;
+        arreglo [4] = 500;
+        arreglo [5] = 600;
+        arreglo [6] = 700;
+        arreglo [7] = 800;
+        arreglo [8] = 900;
+        arreglo [9] = 1000;
+                            //CASTING (Convercion)
+        //System.out.println((int)(Math.random()*100));
+        
+        //ASIGNAR VALORES
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo [i] = (int)(Math.random() * 100 );
+            
+        }
+        //LEER Valores
+        for (int i = 0; i < 10; i++) {
+            System.out.print("[" + arreglo [i] + "]");
+            
+        }
+        System.out.println("");
+    }
+    
+}

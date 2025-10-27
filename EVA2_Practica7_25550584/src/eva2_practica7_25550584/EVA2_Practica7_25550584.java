1f79d53f8ae5293f02e080604391cc3d4bd39da4
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-10-27 15:50:39 -0700

    EVA2_Practica7_25550584

---
+++ EVA2_Practica7_25550584/src/eva2_practica7_25550584/EVA2_Practica7_25550584.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica7_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA2_Practica7_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String cade = "Hola Mundo!!";
+        System.out.println(cade.charAt(0));
+        char caracter = cade.charAt(11);
+        System.out.println(caracter);
+        System.out.println(cade.length());
+        int valor = 10;    
+        
+          }
+    
+}

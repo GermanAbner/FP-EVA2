78885913e5dda42a747b67229611a3c34ab7cc58
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-10-27 15:47:59 -0700

    EVA2_Practica17_25550584

---
+++ EVA2_Practica17_25550584/src/eva2_practica17_25550584/EVA2_Practica17_25550584.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica17_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA2_Practica17_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int i = 5;
+        
+        {
+            System.out.println(i);
+            int valor = 100;
+            System.out.println(valor);
+        }
+        System.out.println(i);
+    }
+    
+}

312bb9b73c1ebbcbb6c6a063181f9ed4509c3933
GermanAbnerRangelLuna <miriamluna.q@gmail.com>
2025-10-28 15:52:57 -0700

    EVA2_Practica18_25550584

---
+++ EVA2_Practica18_25550584/src/eva2_practica18_25550584/EVA2_Practica18_25550584.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica18_25550584;
+
+/**
+ *
+ * @author miria
+ */
+public class EVA2_Practica18_25550584 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 1;
+        while ( i <=10){// se repite si la condicion es verdadera
+                    System.out.println("German Rangel");
+                    i++;
+              
+        }
+        System.out.println("");
+        System.out.println(i);
+        System.out.println("");
+        i = 1;// REINICAR LA VARIABLE SI NO LUEGO SE QUEDA EN 11 Y PS NO QUEDA CHIDO EL PROGGRAAMj
+        do {
+            System.out.println("Rangel Luna");
+            i++;
+        } while (i <= 10);
+    }
+    
+    
+}
